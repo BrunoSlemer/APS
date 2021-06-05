bd89cf3c7d2467aca6a4fe1393e7605515d1fbe9
BrunoSlemer <21445418@anhembimorumbi.edu.br>
2021-06-05 18:05:31 -0300

    detalhes

--- AppConta.java
+++ AppConta.java
@@ -5,7 +5,7 @@ public class AppConta {
       
        //corrente.Depositar(50);
  
-       
+       System.out.println("Teste Conta Corrente");
        if(corrente.Sacar(120) == false){
            System.out.println("saque negado saldo insuficiente: " + corrente.getSaldo());
        }else{
@@ -17,13 +17,16 @@ public class AppConta {
        System.out.println(corrente);
        System.out.println("****************** **************** ******************  ******************");
 
-       ContaPoupança poupança = new ContaPoupança(54321, 200, 3.5);
 
-       
-        if(poupança.Sacar(120) == false){
-        System.out.println("saque negado saldo insuficiente: " + poupança.getSaldo());
+       ContaPoupança poupança = new ContaPoupança(54321, 200, 3.5);
+       poupança.Depositar(50);
+       System.out.println("Teste Conta Poupança");
+       System.out.println("Saldo atual: " + poupança.getSaldo());
+       double valor = 100; // simulando entrada
+        if(poupança.Sacar(valor) == false){
+        System.out.println("saque de "+ valor +" negado saldo insuficiente: " + poupança.getSaldo());
         }else{
-        System.out.println("saque realizado " + "foi atribuida uma taxa de:" + poupança.getTaxa());
+        System.out.println("saque de "+ valor +" realizado " + "foi atribuida uma taxa de:" + poupança.getTaxa());
         }
         System.out.println("Novo saldo Poupanca: " + poupança.getSaldo());
 
